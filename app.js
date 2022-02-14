12f944f020d5f8647d1677dfd55f8bab749b7062
Bisheswar Mahato <42653296+Vishes77@users.noreply.github.com>
2022-02-14 19:29:27 +0530

    Basic Landing Added
    Basic Landing page is added as a '/' Route.

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-app.set('view engine', 'ejs');
-app.set('views',path.join(__dirname,'views'));
 
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.set('view engine', 'ejs');
+app.set('views',path.join(__dirname,'/views'));
 
 //Routes
 app.get('/',(req,res)=>{
-    res.send("Landing Page");
+    res.render('Landing');
 })
 
 app.get('/home',(req,res)=>{
     res.render('home');
 })
+
+// app.get('*',(req,res)=>{
+//     res.send('Please Check Url Address');
+// })
 
 
 
