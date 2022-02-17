16b18e9978ee9b172d760ec492ee82ff07f6a5d0
Bisheswar Mahato <42653296+Vishes77@users.noreply.github.com>
2022-02-17 15:01:26 +0530

    Basic Login & Sign up Added.

--- app.js
+++ app.js
@@ -25,6 +25,14 @@ app.get('/home',(req,res)=>{
     res.render('home');
 })
 
+app.get('/login',(req,res)=>{
+    res.render('login');
+})
+
+app.get('/signup',(req,res)=>{
+    res.render('signup');
+})
+
 app.get('*',(req,res)=>{
     res.send('Please Check Url Address');
 })
